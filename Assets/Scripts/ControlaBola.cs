629dc339fe84a8ec4daede5c15c8ab8dca9f1897
Paloma S Ferreira <103579480+lomaferreira@users.noreply.github.com>
2025-02-02 22:28:58 -0300

    add scene Boliche

---
+++ Assets/Scripts/ControlaBola.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlaBola  : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float power;
+    private bool play=false;
+    public int derrubados;
+    public TMP_Text placar;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        derrubados=0;
+        placar.text = "";
+    
+    }
+
+    void FixedUpdate()
+    {   if(Input.GetKey("a") && transform.position.x < 2.0f && !play){
+            transform.Translate(0.1f,0.0f,0.0f);
+        }
+        if(Input.GetKey("d") && transform.position.x > -2.0f && !play){
+            transform.Translate(-0.1f, 0.0f,0.0f);
+        }
+        if (Input.GetKey("w") && !play)
+        {
+            rb.AddForce(new Vector3(0.0f,0.0f, -Mathf.Abs(power)));
+        }
+
+        if(play){
+            placar.text="Derrubou: " + derrubados.ToString() +" pinos.";
+        }
+    }
+    //PESQUIAR MAIS SOBRE ESSA FUNÇÃO
+     void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pino")) // Verifica se o objeto é um pino
+        {
+            derrubados++;
+            placar.text = "Derrubou: " + derrubados + " pinos";
+            Destroy(other.gameObject); // Remove o pino da cena
+        }
+    }
+
+}
